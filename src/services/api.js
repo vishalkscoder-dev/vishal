9511c2403ba5436bfb5088f9863660ce1a9e3332
Vishal <vishalks.coder@gmail.com>
2026-09-01 19:20:22 +0530

    API fetch Change

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000').replace(/\/$/, '')
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || 'https://vishalbackend-le7f.onrender.com').replace(/\/$/, '')
 
 const api = axios.create({
   baseURL: API_BASE_URL,
